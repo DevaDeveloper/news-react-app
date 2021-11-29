11826dab4635eba42e63aaf895875aac39cf057d
Ognjen <devico997@gmail.com>
2021-11-29 22:53:01 +0100

    development commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <div className={styles.navContent}>
+      <h1>Hello from development branch Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
